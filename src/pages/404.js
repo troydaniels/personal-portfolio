4a471f0680d21f8bceaee98a00c63c7ed1ba6b3b
troydaniels <troyaadaniels@gmail.com>
2019-08-21 23:13:57 +1000

    Conform to AirBnB React styleguide

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,9 +2,9 @@ import React from 'react'
 import { Layout, SEO } from '../components/common'
 
 export default () => (
-	<Layout>
-		<SEO title="404: Not found" location="/404" />
-		<h1>NOT FOUND</h1>
-		<p>There's nothing here...</p>
-	</Layout>
+  <Layout>
+    <SEO title="404: Not found" location="/404" />
+    <h1>NOT FOUND</h1>
+    <p>There's nothing here...</p>
+  </Layout>
 )
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,11 @@ import { Layout, SEO } from '../components/common'
 import { Intro, About, Contact, Projects } from '../components/landing'
 
 export default () => (
-	<Layout>
-		<SEO />
-		<Intro />
-		<Projects />
-		<About />
-		<Contact />
-	</Layout>
+  <Layout>
+    <SEO />
+    <Intro />
+    <Projects />
+    <About />
+    <Contact />
+  </Layout>
 )
