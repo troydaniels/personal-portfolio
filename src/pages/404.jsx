c95025c45ba748bd5ee858b24d5c63604d11747c
troydaniels <troyaadaniels@gmail.com>
2019-08-27 00:01:20 +1000

    Quick & dirty 404 refactor

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import Layout from '../components/common/Layout';
 import SEO from '../components/common/SEO';
+import Header from '../components/theme/Header';
 
 export default () => (
     <Layout>
+        <Header/>
         <SEO title="404: Not found" />
-        <h1>NOT FOUND</h1>
-        <p>There&apso;s nothing here...</p>
+        <div style={{'text-align': 'center'}}>
+            <h1>404 NOT FOUND</h1>
+            <p>There's nothing here...</p>
+        </div>
     </Layout>
 );
