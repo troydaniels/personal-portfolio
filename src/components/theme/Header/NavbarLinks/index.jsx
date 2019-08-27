2b89291fb434e3210626c83d69f9f48cb79ca60b
troydaniels <troyaadaniels@gmail.com>
2019-08-27 13:49:55 +1000

    Add link to CV

--- src/components/theme/Header/NavbarLinks/index.jsx
+++ src/components/theme/Header/NavbarLinks/index.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import * as PropTypes from 'prop-types';
 import AnchorLink from 'react-anchor-link-smooth-scroll';
 import Wrapper from './styles';
+import Resume from '../../../../../static/documents/DANIELStroy.pdf';
 
 const NavbarLinks = ({ desktop }) => (
     <Wrapper desktop={desktop}>
         <AnchorLink href="#about">About</AnchorLink>
         <AnchorLink href="#projects">Projects</AnchorLink>
+        <a href={Resume} target = "_blank">Resume</a>
         <AnchorLink href="#contact">Contact</AnchorLink>
     </Wrapper>
 );
