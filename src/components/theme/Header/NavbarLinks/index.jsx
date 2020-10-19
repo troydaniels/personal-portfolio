2a34814789c981558128c890018373228338dbf9
troydaniels <troyaadaniels@gmail.com>
2020-10-19 12:06:04 +1100

    Readds accidentally removed NavbarLinks/index.jsx

---
+++ src/components/theme/Header/NavbarLinks/index.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import * as PropTypes from 'prop-types';
+import AnchorLink from 'react-anchor-link-smooth-scroll';
+import Wrapper from './styles';
+import Resume from '../../../../../static/documents/DANIELStroy.pdf';
+
+const NavbarLinks = ({ desktop }) => (
+    <Wrapper desktop={desktop}>
+        <AnchorLink href="#about">About</AnchorLink>
+        <AnchorLink href="#projects">Projects</AnchorLink>
+        <a href={Resume} target="_blank" rel="noreferrer">
+            CV
+        </a>
+        <AnchorLink href="#contact">Contact</AnchorLink>
+    </Wrapper>
+);
+
+NavbarLinks.propTypes = {
+    desktop: PropTypes.bool,
+};
+
+NavbarLinks.defaultProps = {
+    desktop: false,
+};
+
+export default NavbarLinks;
